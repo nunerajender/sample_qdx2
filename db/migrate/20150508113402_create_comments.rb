760a1c8a8000380d3a9ff126ca769b7af90416ff
nunerajender <rajender0526@gmail.com>
2015-05-14 08:38:04 +0000

    make it better

--- db/migrate/20150508113402_create_comments.rb
+++ db/migrate/20150508113402_create_comments.rb
@@ -4,9 +4,8 @@ class CreateComments < ActiveRecord::Migration
       t.string :commenter
       t.text :body
       t.references :post, index: true, foreign_key: true
-
       t.timestamps null: false
     end
-    add_foreign_key :comments, :posts
+    #add_foreign_key :comments, :posts
   end
 end
